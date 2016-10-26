9e94b26dbbc97ae77cbe1143e32ffa11e6a3b601
dinusha-b14 <dinushab@jobready.com.au>
2016-10-26 23:49:36 +1100

    created the initial structure for the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './components/todo-list';
+import TodoForm from './components/todo-form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <TodoForm />
         <TodoList />
       </div>
     );
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TodoListItem from './todo-list-item'
 
 class TodoList extends React.Component {
   render() {
     return(
       <div>
-        <p>some content to go here</p>
+        <TodoListItem />
       </div>
     );
   }
