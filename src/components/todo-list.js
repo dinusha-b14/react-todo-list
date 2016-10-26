f08405879c8908c2b1f2af551b2d1037fd09a9b2
dinusha-b14 <dinushab@jobready.com.au>
2016-10-26 22:18:27 +1100

    applied bootstrap styles and updated the readme

---
+++ src/components/todo-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>some content to go here</p>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
